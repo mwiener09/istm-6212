2d43630d4f967aa420cb89774e7a8328649b2755
mwiener09 <mwiener09@gmail.com>
2016-09-20 09:35:05 -0400

    fixed comments and cleaned up
    still need to remove punctuation

--- lwr-rm-stp-wds.py
+++ lwr-rm-stp-wds.py
@@ -8,26 +8,21 @@ import fileinput
 from stop_words import get_stop_words
 import string
 
-
 def format_stopwords():
-# import list of stop words, remove punctuation and lower-case as to match list of words in little women
+    """import list of stop words, lower-case as to match list of words in little women."""
     stop_words = get_stop_words('english')
-    
-        
     stop_words_np = []
     for word in stop_words:
         word = word.lower()
         stop_words_np.append(word)
     return stop_words_np
 
-
 def process(line, stop_words_np):
     """For each line of input, print each line in all lower case letters."""
     w = line.lower().strip()
     if len(w) == 1:
         pass
-    
-    elif not w in stop_words_np:
+        elif not w in stop_words_np:
         print(w)
 
 for line in fileinput.input():
