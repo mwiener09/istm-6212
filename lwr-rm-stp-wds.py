58ed7b02034e29929fc99ed3a1ac95330b02985f
Marissa Wiener <mwiener09@gmail.com>
2016-09-20 00:50:21 -0400

    lower, remove stop words may need to be edited, but others should be correct

---
+++ lwr-rm-stp-wds.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+Transform each line of a text to lower case.
+"""
+
+import fileinput
+from stop_words import get_stop_words
+import string
+
+
+def format_stopwords():
+# import list of stop words, remove punctuation and lower-case as to match list of words in little women
+    stop_words = get_stop_words('english')
+    
+        
+    stop_words_np = []
+    for word in stop_words:
+        word = word.lower()
+        stop_words_np.append(word)
+    return stop_words_np
+
+
+def process(line, stop_words_np):
+    """For each line of input, print each line in all lower case letters."""
+    w = line.lower().strip()
+    
+    if not w in stop_words_np:
+        print(w)
+
+for line in fileinput.input():
+    stopwords = format_stopwords()
+    process(line, stopwords)
